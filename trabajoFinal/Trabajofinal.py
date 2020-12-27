7df592fd8832e1e219a0ff8aaafaaee753e6ff1a
elena <elena@DESKTOP-9V8A583.home>
2020-12-27 22:50:51 +0100

    Cambios 27/12/2020

--- trabajoFinal/Trabajofinal.py
+++ trabajoFinal/Trabajofinal.py
@@ -2,7 +2,7 @@
 # Archivo: xxx.py
 # Autor: Elena Aragoneses de la Rubia y Sergio Bernal Sanchez
 # Fecha: 10/01/2020
-# Descripcion: Ejercicio tranajo final. Realizar tareas de geoprocesamiento (consultas)
+# Descripcion: Ejercicio trabajo final. Realizar tareas de geoprocesamiento (instanciar, proyectar, consultas)
 # con PyQgis
 
 """
@@ -12,9 +12,6 @@ con PyQGIS (consultas tematicas y espaciales)
 """
 
 import qgis, os, ogr
-
-# Importar clase para capas vectoriales
-# porque vamos a trabajar con capas vectoriales
 from qgis.core import *
 from qgis import processing
 
@@ -22,16 +19,25 @@ from qgis import processing
 # No usamos utils.iface.activeLayer porque eso es para seleccionar las layers
 # de la leyenda del proyecto de qgis
 def instanciar(nombre_capa):
+    """
+    Instancia capas vectoriales
+    """
     ruta = directorio + "\\" + nombre_capa
     nombre_instancia = QgsVectorLayer(ruta, nombre_capa, "ogr")
     return nombre_instancia
 
 # Funcion para hacer Join
 def join(instancia_1, instancia_2, campo_join_instancia_1, campo_join_instancia_2):
-    # instancia_1 es la capa en la que se efectua el join
-    # instancia_2 es el otro elemnto que participa en el join
-    # campo_join_instancia_1 es el campo para hacer el join para instancia_1
-    # campo_join_instancia_2 es el campo para hacer el join para instancia_2
+    """
+    Hace el join entre una capa ectorial y tabla o entre
+    2 capas vectoriales
+    
+    Argumetnos: 
+    instancia_1 es la capa en la que se efectua el join
+    instancia_2 es el otro elemnto que participa en el join
+    campo_join_instancia_1 es el campo para hacer el join para instancia_1
+    campo_join_instancia_2 es el campo para hacer el join para instancia_2
+    """
     joinObject = QgsVectorLayerJoinInfo()
     joinObject.setJoinFieldName(campo_join_instancia_2)
     joinObject.setTargetFieldName(campo_join_instancia_1)
@@ -53,8 +59,9 @@ nombre_archivo_csv = "Brussels_AverageAge.csv"
 uri = "file:///" + directorio + "\\" + nombre_archivo_csv + "?delimiter=;"
 # El csv se instancia diferente
 # Se carga como una tabla usado el proveedor delimitedtext
-instancia_csv = QgsVectorLayer(uri, nombre_archivo_csv, "delimitedtext")
-
+# Instancia
+Brussels_AverageAge = QgsVectorLayer(uri, nombre_archivo_csv, "delimitedtext")
+"""
 print ("*" * 20)
 for archivo in lista_archivos:
     if archivo[-4:] == ".shp": # Quedarnos solo con los shp
@@ -66,7 +73,6 @@ for archivo in lista_archivos:
             lista_shp.append(archivo)
             
             capa_sin_extension = archivo[:-4]  # El nombre de la capa sin extension
-            nombre_salida = capa_sin_extension + "_inst"
             
             # Usamos esta funcion en vez de  addVectorLayer() porque no queremos verlas
             # en qgis ahora
@@ -84,7 +90,7 @@ for archivo in lista_archivos:
                 
                 # Necesario decir que la capa de salida es shp (sino formato 
                 # predeterminado: geopackage)
-                nombre_reproyectada = "repr_" + nombre_salida + ".shp"
+                nombre_reproyectada = "repr_" + capa_sin_extension + ".shp"
                 parameter = {"INPUT": archivo, "TARGET_CRS": "EPSG:31370",
                      "OUTPUT": nombre_reproyectada}
                      
@@ -94,10 +100,10 @@ for archivo in lista_archivos:
                 # Esto ha creado una nueva capa, luego se debe instanciar
                 instancia = instanciar(nombre_capa_reproyectada_extension)
         
-                proyeccion = instancia.crs().authid()
+                proyeccion_final = instancia.crs().authid()
                 
-                print ("De {0} se ha creado {1} con {2}".\
-                format(archivo, nombre_capa_reproyectada_extension, proyeccion))
+                print ("De {0} con {1} se ha creado {2} con {3}".\
+                format(archivo, proyeccion, nombre_capa_reproyectada_extension, proyeccion_final))
 
             elif proyeccion == "EPSG:31370":
                 # No hace falta hacer nada si ya tien el EPSG deseado
@@ -105,29 +111,50 @@ for archivo in lista_archivos:
                 
                 print ("La capa {0} ya tenia el EPSG correcto ({1}). No se ha reproyectado."\
                 .format(archivo, proyeccion))
+"""
+print ("*" * 20)
 
 # Siguente paso: hacer joins
 # 1. Entre capa densidad de poblacion y tabla
 # 2. Entre el resultado del anterior join y Average_Income.shp
 # Se deben instanciar diferenciadas las capas que participan en el join
 # El csv ya se instancio arriba de forma diferente
-shp1 = "repr_Population_Density_Brussels_inst.shp"
-instancia_shp1 = instanciar(shp1)
-shp2 = "Average_Income.shp"
-instancia_shp2 = instanciar(shp2)
+shp1 = "repr_Population_Density_Brussels.shp"
+Population_Density_Brussels = instanciar(shp1)
+shp2 = "repr_Average_Income.shp"
+Average_Income = instanciar(shp2)
 
 # Campos para hacer los join
 shpField="MUNC"
 csvField="Munc"
-# Hacer el join
-join(instancia_shp1, instancia_csv, shpField, csvField)
-
-# Es necesario instanciar esta capa para usarla en el siguiente join
-join(instancia_shp1, instancia_shp2, shpField, shpField)
+# Hacer los joins (se concatenan y se alamacena todo en la misma capa)
+join(Population_Density_Brussels, Brussels_AverageAge, shpField, csvField)
+join(Population_Density_Brussels, Average_Income, shpField, shpField)
 
 # Se puede observar como se han añadido nuevos campos de la tabla (ejemplo con join 1)
-print ("Campos tras el join:")
-for field in instancia_shp1.fields():
+print ("Campos tras el join en {0}:".format(shp1))
+for field in Population_Density_Brussels.fields():
     print(field.name(), field.typeName())
 
-# Seleccion pro atributos    
+# SELECCIONES
+
+# Seleccion por atributos
+# Seleccion 1. Seleccion de carreteras rapidas y ferrocarriles de Bruselas
+# Necesito instanciar la capa de Bruselas
+shp3 = "BrusselsCity.shp"
+Bruselas = instanciar(shp3)
+
+# Condicion para la seleccion. Se usa or porque puede ser ambas
+exp_selec_1 = "\"ITEM\" = 'Railways and associated land' or \"ITEM\" = 'Fast transit roads and associated land'"
+Bruselas.selectByExpression(exp_selec_1, QgsVectorLayer.SetSelection)
+# Exporto la seleccion a una capa
+ruta_salida = directorio + "\\" + "RoadRailways.shp"
+encoding = "windows-1252"
+QgsVectorFileWriter.writeAsVectorFormat(Bruselas, ruta_salida,encoding, Bruselas.crs(), "ESRI Shapefile", onlySelected=True)
+
+# Para borrar la seleccion
+# Bruselas.removeSelection()
+# Pero en este caso no se usa porque queremos "concatenar" selecciones
+
+# Seleccion 2. Seleccion de caracteristicas de la poblacion
+# del resultado del join
